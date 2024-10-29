12de050a14db512ee3a10b29b13c62acf3c39d92
Allie Caughman <acaughman@ucsb.edu>
2024-10-29 09:57:54 -0700

    add first pass at figures

--- scripts/04_sim_analysis.R
+++ scripts/04_sim_analysis.R
@@ -52,29 +52,33 @@ mpa <- mpa %>%
 
 connect <- connect_full %>%
   mutate(
-    adult_fished_c_1 = adult_RS_fished + adult_IS_fished,
-    adult_mpa_c_1 = adult_RS_mpa + adult_IS_mpa,
-    larvae_fished_c_1 = larvae_RS_fished + larvae_IS_fished,
-    larvae_mpa_c_1 = larvae_RS_mpa + larvae_IS_mpa,
-    adult_c_2 = adult_SR + adult_II,
-    larvae_c_2 = larvae_SR + larvae_II,
+    adult_fished_abs = adult_RS_fished + adult_IS_fished,
+    adult_mpa_abs = adult_RS_mpa + adult_IS_mpa,
+    larvae_fished_abs = larvae_RS_fished + larvae_IS_fished,
+    larvae_mpa_abs = larvae_RS_mpa + larvae_IS_mpa,
+    adult_rel = adult_SR + adult_II,
+    larvae_rel = larvae_SR + larvae_II,
     movement = c(paste0(adult, " / ", larval))
   ) %>%
   mutate(
-    fished_c1 = adult_fished_c_1 + larvae_fished_c_1,
-    mpa_c1 = adult_mpa_c_1 + larvae_mpa_c_1,
-    c2 = adult_c_2 + larvae_c_2,
-    fished_c3 = adult_ES_fished + larvae_ES_fished,
-    mpa_c3 = adult_ES_mpa + larvae_ES_mpa,
-    relative_mpa_c1 = adult_mpa_c_1 / larvae_mpa_c_1,
-    relative_fished_c1 = adult_fished_c_1 / larvae_fished_c_1,
-    relative_c2 = adult_c_2 / larvae_c_2,
+    fished_abs = adult_fished_abs + larvae_fished_abs,
+    mpa_abs = adult_mpa_abs + larvae_mpa_abs,
+    rel = adult_rel + larvae_rel,
+    fished_export = adult_ES_fished + larvae_ES_fished,
+    mpa_export = adult_ES_mpa + larvae_ES_mpa,
+    relative_mpa_abs = adult_mpa_abs / larvae_mpa_abs,
+    relative_fished_abs = adult_fished_abs / larvae_fished_abs,
+    relative_rel = adult_rel / larvae_rel,
     relative_mpa_R = adult_RS_mpa / larvae_RS_mpa,
     relative_fished_R = adult_RS_fished / larvae_RS_fished,
     relative_mpa_I = adult_IS_mpa / larvae_IS_mpa,
     relative_fished_I = adult_IS_fished / larvae_IS_fished,
     relative_mpa_E =  adult_ES_mpa / larvae_ES_mpa,
-    relative_fished_E = adult_ES_fished / larvae_ES_fished
+    relative_fished_E = adult_ES_fished / larvae_ES_fished,
+    adult_relative_mpa = adult_RS_mpa / adult_IS_mpa,
+    adult_relative_fished = adult_RS_fished / adult_IS_fished,
+    larvae_relative_mpa = larvae_RS_mpa / larvae_IS_mpa,
+    larvae_relative_fished = larvae_RS_fished / larvae_IS_fished,
   ) %>%
   mutate(adult_cat = case_when(
     adult %in% c(2) ~ "low",
@@ -110,6 +114,166 @@ connect <- connect_full %>%
 eq_pop_size = mpa %>% 
   filter(age == "adult") %>% 
   filter(generation == 90)
+
+
+# Theoretical vs Realized Connectivity ------------------------------------
+
+sub_connect = connect %>% 
+  filter(mpa_size %in% c(4, 8)) %>% 
+  filter(mpa_spacing == 8) %>% 
+  filter(eggs == "low") %>% 
+  filter(fp == "high")
+  
+p1 = ggplot(sub_connect) +
+  geom_hline(aes(yintercept = 1), color = "red", alpha = 0.5, linetype = "dashed") +
+  geom_point(aes(adult, relative_rel, color = as.factor(larval), shape = as.factor(mpa_size)), size = 3) +
+  geom_line(aes(adult, relative_rel, color = as.factor(larval), linetype = as.factor(mpa_size)), linewidth = 1) +
+  theme_bw() +
+  scale_color_viridis_d(end = 0.9) +
+  labs(x = "Adult Movement",
+       y = "Relative Connectivity (Adult / Larval)",
+       color = "Larval Movement",
+       shape = "MPA Size",
+       linetype = "MPA Size") +
+  ylim(c(0.2, 8.5))
+
+p2 = ggplot(sub_connect) +
+  geom_hline(aes(yintercept = 1), color = "red", alpha = 0.5, linetype = "dashed") +
+  geom_point(aes(adult, relative_fished_abs, color = as.factor(larval), shape = as.factor(mpa_size)), size = 3) +
+  geom_line(aes(adult, relative_fished_abs, color = as.factor(larval), linetype = as.factor(mpa_size)), linewidth = 1) +
+  theme_bw() +
+  scale_color_viridis_d(end = 0.9) +
+  labs(x = "Adult Movement",
+       y = "",
+       color = "Larval Movement",
+       shape = "MPA Size",
+       linetype = "MPA Size") +
+  ylim(c(0.2, 8.5))
+
+p3 = ggplot(sub_connect) +
+  geom_hline(aes(yintercept = 1), color = "red", alpha = 0.5, linetype = "dashed") +
+  geom_point(aes(adult, relative_mpa_abs, color = as.factor(larval), shape = as.factor(mpa_size)), size = 3) +
+  geom_line(aes(adult, relative_mpa_abs, color = as.factor(larval), linetype = as.factor(mpa_size)), linewidth = 1) +
+  theme_bw() +
+  scale_color_viridis_d(end = 0.9) +
+  labs(x = "Adult Movement",
+       y = "",
+       color = "Larval Movement",
+       shape = "MPA Size",
+       linetype = "MPA Size") +
+  ylim(c(0.2, 8.5))
+
+# May Drop the Size bit 
+p = p1 + p2 + p3 + plot_annotation(tag_levels = "A") + plot_layout(guides = "collect")
+
+ggsave(p, path = here::here("figs"), file = paste0("fig1.pdf"), height = 8, width = 20)
+
+# Fishing Pressure and Connectivity ---------------------------------------
+
+sub_connect = connect %>% 
+  filter(mpa_size == 8) %>% 
+  filter(mpa_spacing %in% c(0,8)) %>% 
+  filter(eggs == "med")
+
+colors = c("Retention" = "#35b779", "Import" = "#440154", "Export" = "#31688e", "Retention + Import" = "#21918c")
+
+p1 = ggplot(sub_connect %>% filter(mpa_spacing == 0)) +
+  geom_hline(aes(yintercept = 1), color = "red", alpha = 0.5, linetype = "dashed") +
+  geom_rect(aes(xmin = 0.5, xmax = 3.5, ymin = 0, ymax = 16, color = "Export"), fill = "transparent", linetype = "dashed", linewidth = 0.2) +  
+  geom_rect(aes(xmin = 6.5, xmax = 9.5, ymin = 0, ymax = 16, color = "Retention"), fill = "transparent", linetype = "dashed", linewidth = 0.2) +
+  geom_point(aes(fct_reorder(move_cat, relative_mpa_R, .desc = TRUE), relative_mpa_R, color = "Retention"), size = 3) +
+  geom_point(aes(move_cat, relative_mpa_I, color = "Import"), size = 3) +
+  geom_point(aes(move_cat, relative_mpa_E, color = "Export"), size = 3) +
+  theme_bw() +
+  facet_wrap(~fp) +
+  theme(strip.text = element_text(face = "bold"),
+        strip.background = element_rect(fill = "white"),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x = "", 
+       y = "Relative Connectivity (Adult / Larval)",
+       color = "") +
+  ylim(c(NA, 16)) +
+  scale_color_manual(values = colors)
+
+p2 = ggplot(sub_connect %>% filter(mpa_spacing == 8)) +
+  geom_hline(aes(yintercept = 1), color = "red", alpha = 0.5, linetype = "dashed") +
+  geom_rect(aes(xmin = 0.5, xmax = 3.5, ymin = 0, ymax = 16, color = "Export"), fill = "transparent", linetype = "dashed", linewidth = 0.2) +  
+  geom_rect(aes(xmin = 4.5, xmax = 5.5, ymin = 0, ymax = 16, color = "Export"), fill = "transparent", linetype = "dashed", linewidth = 0.2) + 
+  geom_rect(aes(xmin = 6.5, xmax = 9.5, ymin = 0, ymax = 16, color = "Retention"), fill = "transparent", linetype = "dashed", linewidth = 0.2) +
+  geom_point(aes(fct_reorder(move_cat, relative_mpa_R, .desc = TRUE), relative_mpa_R, color = "Retention"), size = 3) +
+  geom_point(aes(move_cat, relative_mpa_I, color = "Import"), size = 3) +
+  geom_point(aes(move_cat, relative_mpa_E, color = "Export"), size = 3) +
+  theme_bw() +
+  facet_wrap(~fp) +
+  theme(strip.text = element_text(face = "bold"),
+        strip.background = element_rect(fill = "white"),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x = "Movement (Adult / Larval)", 
+       y = "Relative Connectivity (Adult / Larval)",
+       color = "") +
+  ylim(c(NA, 16)) +
+  scale_color_manual(values = colors) +
+  theme(legend.position = "none")
+
+# may drop to just spacing == 8 if patterns are the same
+p = p1 / p2 + plot_annotation(tag_levels = "A") + plot_layout(guides = "collect")
+
+ggsave(p, path = here::here("figs"), file = paste0("fig2.pdf"), height = 15, width = 8)
+
+# Shifts in Connectivity Driver -------------------------------------------
+
+sub_connect = connect %>% 
+  filter(eggs == "med") %>% 
+  filter(mpa_size %in% c(4, 8)) %>% 
+  filter(move_cat %in% c("high / low",  "high / medium", "medium / low")) %>% 
+  pivot_longer(cols = c(relative_mpa_abs, relative_mpa_R), names_to = "measure", values_to = "connectivity") %>% 
+  mutate(measure = case_when(
+    measure == "relative_mpa_R" ~ "Retention",
+    measure == "relative_mpa_abs" ~ "Retention + Import"
+  )) %>% 
+  mutate(move_cat = fct_relevel(move_cat, c("medium / low", "high / medium", "high / low")))
+
+p1 = ggplot(sub_connect %>% filter(connectivity < 50)) +
+  geom_hline(aes(yintercept = 1), color = "red", alpha = 0.5, linetype = "dashed") +
+  geom_point(aes(move_cat, connectivity, color = as.factor(mpa_spacing), shape = as.factor(mpa_size)), size = 2) +
+  theme_bw() +
+  labs(x = "Movement (Adult / Larval)", 
+       y = "Relative Connectivity (Adult / Larval)",
+       color = "MPA Spacing",
+       shape = "MPA Size") +
+  facet_wrap(fp~measure, scales = "free", ncol = 2) +
+  scale_color_viridis_d(end = 0.9) +
+  theme(strip.text = element_text(face = "bold"),
+        strip.background = element_rect(fill = "white"),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggsave(p1, path = here::here("figs"), file = paste0("fig3.pdf"), height = 8, width = 8)
+
+# In/Out Ratio ------------------------------------------------------------
+
+eq_pop_size_sub = eq_pop_size %>% 
+  filter(mpa_size %in% c(4, 8)) %>% 
+  filter(fp == "high") %>% 
+  filter(eggs == "low")
+
+p1 = ggplot(eq_pop_size_sub) +
+  geom_hline(aes(yintercept = 0), color = "red", alpha = 0.5, linetype = "dashed") +
+  # geom_hline(aes(yintercept = 0.7), color = "red", alpha = 0.5, linetype = "dashed") +
+  geom_point(aes(mpa_spacing, in_out, color = move_cat, shape = move_cat), size = 2) +
+  facet_wrap(~mpa_size) +
+  theme_bw() +
+  theme(strip.text = element_text(face = "bold"),
+        strip.background = element_rect(fill = "white")) +
+  labs(x = "MPA Spacing",
+       y = "Population",
+       color = "Movement (Adult / Larval)",
+       shape = "Movement (Adult / Larval)") +
+  scale_color_viridis_d(end = 0.9) +
+  scale_shape_manual(values = c(1, 19, 1, 19, 19, 19, 19, 1, 19))
+
+ggsave(p1, path = here::here("figs"), file = paste0("fig4.pdf"), height = 8, width = 8)
+
+# OLD ---------------------------------------------------------------------
 
 
 # ggplot(eq_pop_size) +
