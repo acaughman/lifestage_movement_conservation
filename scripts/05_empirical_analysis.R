68b01bd3aad1b546a895e4582a92fa8cc9412db6
Allie Caughman <acaughman@ucsb.edu>
2025-03-28 12:41:04 -0700

    get averages

--- scripts/05_empirical_analysis.R
+++ scripts/05_empirical_analysis.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(patchwork)
 
-
 # RF data ---------------------------------------------------------
 
 rf = read_csv(here::here("data", "raw_data", "rf_data.csv")) %>% 
@@ -26,22 +25,33 @@ full_pisco <- read_csv(here::here("data", "raw_data", "full_pisco.csv")) %>%
   filter(level != "CAN") %>% 
   dplyr::select(-lat_c, -lon_c, -latitude, -longitude) %>%   #remove lat, long for site maps below
   distinct() %>% 
-  group_by(year, site, affiliated_mpa, site_status, zone, transect) %>% 
-  mutate(total_count = sum(count),
-         total_biomass = sum(weight_kg)) %>% 
-  replace_na(list(total_biomass = 0, count = 0)) %>% 
-  ungroup() %>% 
-  dplyr::select(-level, -weight_kg, -length_to_use, -weight_g, -date, -day, -month) %>% 
+  # get average per transect in each MPA and ref
+  group_by(year, affiliated_mpa, sciname, site_status) %>% 
+  mutate(count = mean(count, na.rm=TRUE),
+         biomass = mean(weight_kg, na.rm=TRUE)) %>% 
+  replace_na(list(biomass_per = 0, count_per = 0)) %>% 
+  ungroup()%>% 
+  dplyr::select(-weight_kg, -length_to_use, -weight_g, -date, -day, -month,-zone,-transect, -level, -site , -region4, -temp) %>% 
   distinct() %>% 
   filter(!is.na(sciname))%>% 
   filter(!grepl("spp", sciname)) %>% 
-  group_by(sciname, year, affiliated_mpa, mpa_defacto_designation) %>% 
-  mutate(biomass = mean(total_biomass, na.rm=TRUE),
-         count = mean(total_count, na.rm = TRUE),
-         n_rep = n()) %>% 
-  ungroup() %>% 
-  dplyr::select(-site, -zone,-transect, -total_biomass, -total_count) %>% 
-  distinct()
+  #remove data from within 2 years of MPA implementation
+  mutate(imp_year = year(mdy(implementation_date))) %>% 
+  filter(year > imp_year + 2)%>% 
+  #remove outliers within years
+  group_by(sciname) %>% 
+  mutate(q1 = quantile(count, probs = 0.25),
+         q3 = quantile(count, probs = 0.75)) %>% 
+  filter(count < q3 + (2.5 * (q3 - q1))) %>% 
+  filter(count > q1 - (2.5 * (q3 - q1))) %>% 
+  mutate(species_count = n()) %>% 
+  filter(species_count > 10) %>% 
+  select(-species_count, -q1, -q3, -imp_year, -implementation_date) %>% 
+  # calcluate year average
+  group_by(affiliated_mpa, sciname, site_status) %>% 
+  mutate(avg_count = mean(count, na.rm=TRUE),
+         avg_biomass = mean(biomass, na.rm=TRUE)) 
+  
 
 pisco_rf = left_join(rf, full_pisco) %>% 
   filter(!is.na(affiliated_mpa))
@@ -80,38 +90,47 @@ sub_pisco = full_pisco %>%
     sciname == "Sebastes auriculatus" ~ "med HR, high PLD",
     sciname == "Sebastes melanops" ~ "med HR, high PLD",
     sciname == "Sebastes melanostomus"~ "med HR, high PLD",
-    sciname == "Sebastes mystinus" ~ "high HR, high PLD",
+    sciname == "Sebastes mystinus" ~ "med HR, high PLD",
     sciname == "Trachurus symmetricus" ~ "high HR, high PLD",
     sciname == "Hypsypops rubicundus" ~ "low HR, high PLD",
     sciname == "Sebastes serriceps"~ "low HR, high PLD",
     sciname == "Embiotoca jacksoni" ~ "low HR, low PLD",
     !is.na(hr_cat) & !is.na(pld_cat) ~ paste0(hr_cat, ", ", pld_cat)
   )) %>% 
+  filter(!is.na(movement)) %>% 
   mutate(affiliated_mpa = fct_reorder(affiliated_mpa, min_dist, .desc = FALSE)) %>% 
   mutate(mpa_designation = case_when(
     mpa_defacto_designation == "ref" ~ "reference",
     TRUE ~ "MPA"
   )) %>% 
   dplyr::select(-mpa_defacto_designation, -mpa_state_designation) %>% 
-  filter(movement != "high HR, low PLD") %>% 
-  mutate(movement = fct_relevel(movement, "low HR, low PLD", "low HR, high PLD", "med HR, low PLD", "med HR, high PLD", "high HR, high PLD"))
+  filter(!(movement %in% c("high HR, low PLD", "med HR, high PLD"))) %>% 
+  mutate(movement = fct_relevel(movement, "low HR, low PLD", "low HR, high PLD", "med HR, low PLD", "med HR, high PLD", "high HR, high PLD"))# %>% 
+  # filter(sciname != "Scomber japonicus")
 
-mpa_sites = sub_pisco %>% 
-  filter(mpa_designation == "MPA") %>% 
-  distinct() %>% 
-  rename(mpa_count = count) %>% 
-  dplyr::select(-mpa_designation, -biomass, -n_rep)
-ref_sites = sub_pisco %>% 
-  filter(mpa_designation == "reference") %>% 
-  distinct() %>% 
-  rename(ref_count = count) %>% 
-  dplyr::select(-mpa_designation, -biomass, -n_rep)
+mpa_sites = sub_pisco %>%
+  filter(mpa_designation == "MPA") %>%
+  distinct() %>%
+  ungroup() %>% 
+  rename(mpa_count = avg_count) %>%
+  dplyr::select(-mpa_designation, -biomass, -year, -count, - avg_biomass, -site_status) %>% 
+  distinct()
+ref_sites = sub_pisco %>%
+  filter(mpa_designation == "reference") %>%
+  distinct() %>%
+  ungroup() %>% 
+  rename(ref_count = avg_count) %>%
+  dplyr::select(-mpa_designation, -biomass, -year, -count, -avg_biomass, -site_status) %>%
+  distinct()
 
-wider_pisco = full_join(mpa_sites, ref_sites) %>% 
-  mutate(in_out = mpa_count / ref_count)
+wider_pisco = full_join(mpa_sites, ref_sites) %>%
+  mutate(in_out = log(mpa_count / ref_count)) %>% 
+  filter(!is.na(in_out)) %>% 
+  select(sciname, affiliated_mpa, in_out)
 
 mpa_summary = full_pisco %>% 
-  dplyr::select(affiliated_mpa, implementation_date,  size, min_dist, max_dist) %>% 
+  ungroup() %>% 
+  dplyr::select(affiliated_mpa, size, min_dist, max_dist) %>% 
   distinct() %>% 
   mutate(larvae_spacing = case_when(
    min_dist > 192 ~ "not close enough",
@@ -138,13 +157,23 @@ mpa_summary = full_pisco %>%
     size >= 32 * 2 ~ "large enough for all"
   ))
 
-sub_pisco = full_join(sub_pisco, mpa_summary)
+sub_pisco = list(sub_pisco, mpa_summary, wider_pisco) %>% 
+  reduce(full_join)
 
 sub_pisco %>% 
+  ungroup() %>% 
   select(sciname, movement) %>% 
   distinct() %>% 
   group_by(movement) %>% 
   summarize(n = n())
+
+ggplot(sub_pisco %>% filter(site_status == "MPA")) +
+  geom_line(aes(year, count, color = movement)) +
+  theme_bw() +
+  facet_wrap(~sciname)
+
+
+# count figs --------------------------------------------------------------
 
 p1 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(movement, count, color = movement)) +
   geom_boxplot() +
@@ -160,34 +189,107 @@ p1 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(movement, count, co
   facet_wrap(~affiliated_mpa)
 p1
 
-move_avg = sub_pisco %>% 
-  filter(site_status == "MPA") %>% 
-  group_by(movement, affiliated_mpa) %>% 
-  mutate(avg_count = mean(count, na.rm=TRUE))
+p1 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(sciname, count, color = movement)) +
+  geom_boxplot() +
+  theme_bw() +
+  scale_y_log10() +
+  theme(strip.background = element_rect(fill = "transparent")) +
+  scale_color_viridis_d() +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank()
+  ) +
+  labs(y = "Count/Transect", x = "", color = "MPA") +
+  facet_wrap(~affiliated_mpa)
+p1
 
-p2 <- ggplot(move_avg, aes(size * prop_rock, avg_count, color = movement)) +
+p2 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(size, count, color = movement)) +
+  geom_vline(aes(xintercept = 0.5 * 2), color = "red", linetype = "dashed")+
+  geom_vline(aes(xintercept = 8 * 2), color = "red", linetype = "dashed")+
+  geom_vline(aes(xintercept = 32 * 2), color = "red", linetype = "dashed")+
+  # geom_line() +
+  geom_smooth(method= "lm") +
+  theme_bw() +
+  theme(strip.background = element_rect(fill = "transparent")) +
+  scale_color_viridis_d(end = 0.9) +
+  facet_wrap(~movement, scales = "free_y") +
+  labs(y = "Count/Transect", x = "", color = "MPA")
+p2
+
+ggsave(p2, filename = here::here("figs", "empirical_count_size.pdf"), height = 12, width = 12)
+
+p2 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(min_dist, count, color = movement)) +
   geom_vline(aes(xintercept = 0.5), color = "red", linetype = "dashed")+
   geom_vline(aes(xintercept = 8), color = "red", linetype = "dashed")+
-  # geom_vline(aes(xintercept = 32), color = "red", linetype = "dashed")+
-  geom_line() +
-  geom_smooth(method = "lm") +
+  geom_vline(aes(xintercept = 32), color = "red", linetype = "dashed")+
+  # geom_line() +
+  geom_smooth(method= "lm") +
   theme_bw() +
-  scale_y_log10() +
   theme(strip.background = element_rect(fill = "transparent")) +
-  scale_color_viridis_d() +
+  scale_color_viridis_d(end = 0.9) +
+  facet_wrap(~movement, scales = "free_y") +
   labs(y = "Count/Transect", x = "", color = "MPA")
 p2
 
-p2 <- ggplot(move_avg, aes(min_dist, avg_count, color = movement)) +
-  # geom_vline(aes(xintercept = 0.5), color = "red", linetype = "dashed")+
+ggsave(p2, filename = here::here("figs", "empirical_cout_space.pdf"), height = 12, width = 12)
+
+
+# in/out figs --------------------------------------------------------------
+
+p1 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(movement, in_out, color = movement)) +
+  geom_boxplot() +
+  theme_bw() +
+  # scale_y_log10() +
+  theme(strip.background = element_rect(fill = "transparent")) +
+  scale_color_viridis_d() +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank()
+  ) +
+  labs(y = "Count/Transect", x = "", color = "MPA") +
+  facet_wrap(~affiliated_mpa)
+p1
+
+p1 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(sciname, in_out, color = movement)) +
+  geom_boxplot() +
+  theme_bw() +
+  # scale_y_log10() +
+  theme(strip.background = element_rect(fill = "transparent")) +
+  scale_color_viridis_d() +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank()
+  ) +
+  labs(y = "Count/Transect", x = "", color = "MPA") +
+  facet_wrap(~affiliated_mpa)
+p1
+
+p2 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(size, in_out, color = movement)) +
+  geom_vline(aes(xintercept = 0.5 * 2), color = "red", linetype = "dashed")+
+  geom_vline(aes(xintercept = 8 * 2), color = "red", linetype = "dashed")+
+  geom_vline(aes(xintercept = 32 * 2), color = "red", linetype = "dashed")+
+  # geom_line() +
+  geom_smooth(method = "lm") +
+  theme_bw() +
+  theme(strip.background = element_rect(fill = "transparent")) +
+  scale_color_viridis_d(end = 0.9) +
+  facet_wrap(~movement, scales = "free_y") +
+  labs(y = "Count/Transect", x = "", color = "MPA")
+p2
+
+ggsave(p2, filename = here::here("figs", "empirical_inout_size.pdf"), height = 12, width = 12)
+
+p2 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(min_dist, in_out, color = movement)) +
+  geom_vline(aes(xintercept = 0.5), color = "red", linetype = "dashed")+
   geom_vline(aes(xintercept = 8), color = "red", linetype = "dashed")+
   geom_vline(aes(xintercept = 32), color = "red", linetype = "dashed")+
-  geom_line() +
-  geom_smooth(method = "lm") +
+  # geom_line() +
+  geom_smooth(method= "lm") +
   theme_bw() +
-  scale_y_log10() +
   theme(strip.background = element_rect(fill = "transparent")) +
-  scale_color_viridis_d() +
+  scale_color_viridis_d(end = 0.9) +
+  facet_wrap(~movement, scales = "free_y") +
   labs(y = "Count/Transect", x = "", color = "MPA")
 p2
 
+ggsave(p2, filename = here::here("figs", "empirical_inout_space.pdf"), height = 12, width = 12)
