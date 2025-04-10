21b1d8f8f6af04228701a41371dbde074f279d1c
Allie Caughman <acaughman@ucsb.edu>
2025-04-09 21:49:10 -0700

    rename

---
+++ scripts/06_empirical_analysis.R
@@ -0,0 +1,327 @@
+library(tidyverse)
+library(patchwork)
+library(sf)
+
+# RF data ---------------------------------------------------------
+
+rf <- read_csv(here::here("data", "raw_data", "rf_data.csv")) %>%
+  mutate(pld_dist = ((pi * (1.33 * (pld)^1.3)^2)) / 100 / 2) %>%
+  filter(!is.na(homerange)) %>%
+  filter(!is.na(pld)) %>%
+  mutate(dominate = case_when(
+    round(homerange, 2) < round(pld_dist, 2) ~ "PLD",
+    round(homerange, 2) >= round(pld_dist, 2) ~ "Home range"
+  ))
+
+dominate_sum <- rf %>%
+  group_by(dominate) %>%
+  summarize(count = n())
+
+percent_dominate_adult <- dominate_sum$count[1] / sum(dominate_sum$count) * 100
+
+# Data Load and Initial Manipulation --------------------------------------
+
+full_pisco <- read_csv(here::here("data", "raw_data", "full_pisco.csv")) %>%
+  distinct() %>%
+  filter(level != "CAN") %>%
+  dplyr::select(-lat_c, -lon_c, -latitude, -longitude) %>% # remove lat, long for site maps below
+  distinct() %>%
+  # get average per transect in each MPA and ref
+  group_by(year, affiliated_mpa, sciname, site_status) %>%
+  mutate(
+    count = mean(count, na.rm = TRUE),
+    biomass = mean(weight_kg, na.rm = TRUE)
+  ) %>%
+  replace_na(list(biomass_per = 0, count_per = 0)) %>%
+  ungroup() %>%
+  dplyr::select(-weight_kg, -length_to_use, -weight_g, -date, -day, -month, -zone, -transect, -level, -site, -region4, -temp) %>%
+  distinct() %>%
+  filter(!is.na(sciname)) %>%
+  filter(!grepl("spp", sciname)) %>%
+  # remove data from within 2 years of MPA implementation
+  mutate(imp_year = year(mdy(implementation_date))) %>%
+  filter(year > imp_year + 2) %>%
+  # remove outliers within years
+  group_by(sciname) %>%
+  mutate(
+    q1 = quantile(count, probs = 0.25),
+    q3 = quantile(count, probs = 0.75)
+  ) %>%
+  filter(count < q3 + (2.5 * (q3 - q1))) %>%
+  filter(count > q1 - (2.5 * (q3 - q1))) %>%
+  mutate(species_count = n()) %>%
+  filter(species_count > 10) %>%
+  select(-species_count, -q1, -q3, -imp_year, -implementation_date) %>%
+  # calcluate year average
+  group_by(affiliated_mpa, sciname, site_status) %>%
+  mutate(
+    avg_count = mean(count, na.rm = TRUE),
+    avg_biomass = mean(biomass, na.rm = TRUE)
+  )
+
+
+pisco_rf <- left_join(rf, full_pisco) %>%
+  filter(!is.na(affiliated_mpa))
+
+sub_pisco <- full_pisco %>%
+  filter(sciname %in% c(
+    "Paralabrax clathratus",
+    "Medialuna californiensis",
+    "Ophiodon elongatus",
+    "Acanthoclinus fuscus",
+    "Phanerodon furcatus",
+    "Sebastes auriculatus",
+    "Sebastes melanops",
+    "Sebastes melanostomus",
+    "Sebastes mystinus",
+    "Trachurus symmetricus",
+    "Hypsypops rubicundus",
+    "Sebastes serriceps",
+    "Embiotoca jacksoni"
+  )) %>%
+  full_join(pisco_rf) %>%
+  filter(target_status == "Targeted") %>%
+  mutate(hr_cat = case_when(
+    magnitude_homerange < 0 ~ "low HR",
+    magnitude_homerange > 1 ~ "high HR",
+    !is.na(magnitude_homerange) ~ "med HR"
+  )) %>%
+  mutate(pld_cat = case_when(
+    month_pld == 0 ~ "low PLD",
+    month_pld > 0 ~ "high PLD"
+  )) %>%
+  mutate(movement = case_when(
+    sciname == "Paralabrax clathratus" ~ "low HR, high PLD",
+    sciname == "Medialuna californiensis" ~ "med HR, high PLD",
+    sciname == "Ophiodon elongatus" ~ "med HR, high PLD",
+    sciname == "Acanthoclinus fuscus" ~ "low HR, high PLD",
+    sciname == "Phanerodon furcatus" ~ "med HR, low PLD",
+    sciname == "Sebastes auriculatus" ~ "med HR, high PLD",
+    sciname == "Sebastes melanops" ~ "med HR, high PLD",
+    sciname == "Sebastes melanostomus" ~ "med HR, high PLD",
+    sciname == "Sebastes mystinus" ~ "med HR, high PLD",
+    sciname == "Trachurus symmetricus" ~ "high HR, high PLD",
+    sciname == "Hypsypops rubicundus" ~ "low HR, high PLD",
+    sciname == "Sebastes serriceps" ~ "low HR, high PLD",
+    sciname == "Embiotoca jacksoni" ~ "low HR, low PLD",
+    !is.na(hr_cat) & !is.na(pld_cat) ~ paste0(hr_cat, ", ", pld_cat)
+  )) %>%
+  mutate(hr_cat = case_when(
+    sciname == "Paralabrax clathratus" ~ "low HR",
+    sciname == "Medialuna californiensis" ~ "med HR",
+    sciname == "Ophiodon elongatus" ~ "med HR",
+    sciname == "Acanthoclinus fuscus" ~ "low HR",
+    sciname == "Phanerodon furcatus" ~ "med HR",
+    sciname == "Sebastes auriculatus" ~ "med HR",
+    sciname == "Sebastes melanops" ~ "med HR",
+    sciname == "Sebastes melanostomus" ~ "med HR",
+    sciname == "Sebastes mystinus" ~ "med HR",
+    sciname == "Trachurus symmetricus" ~ "high HR",
+    sciname == "Hypsypops rubicundus" ~ "low HR",
+    sciname == "Sebastes serriceps" ~ "low HR",
+    sciname == "Embiotoca jacksoni" ~ "low HR",
+    !is.na(hr_cat) ~ hr_cat
+  )) %>%
+  filter(!is.na(movement)) %>%
+  mutate(affiliated_mpa = fct_reorder(affiliated_mpa, min_dist, .desc = FALSE)) %>%
+  mutate(mpa_designation = case_when(
+    mpa_defacto_designation == "ref" ~ "reference",
+    TRUE ~ "MPA"
+  )) %>%
+  dplyr::select(-mpa_defacto_designation, -mpa_state_designation) %>%
+  filter(!(movement %in% c("high HR, low PLD", "med HR, high PLD"))) %>%
+  mutate(movement = fct_relevel(movement, "low HR, low PLD", "low HR, high PLD", "med HR, low PLD", "med HR, high PLD", "high HR, high PLD")) %>%
+  mutate(hr_cat = fct_relevel(hr_cat, "low HR", "high HR"))
+
+mpa_sites <- sub_pisco %>%
+  filter(mpa_designation == "MPA") %>%
+  distinct() %>%
+  ungroup() %>%
+  rename(mpa_count = avg_count) %>%
+  dplyr::select(-mpa_designation, -biomass, -year, -count, -avg_biomass, -site_status) %>%
+  distinct()
+ref_sites <- sub_pisco %>%
+  filter(mpa_designation == "reference") %>%
+  distinct() %>%
+  ungroup() %>%
+  rename(ref_count = avg_count) %>%
+  dplyr::select(-mpa_designation, -biomass, -year, -count, -avg_biomass, -site_status) %>%
+  distinct()
+
+wider_pisco <- full_join(mpa_sites, ref_sites) %>%
+  mutate(in_out = log(mpa_count / ref_count)) %>%
+  filter(!is.na(in_out)) %>%
+  select(sciname, affiliated_mpa, in_out)
+
+mpa_summary <- full_pisco %>%
+  ungroup() %>%
+  dplyr::select(affiliated_mpa, size, min_dist, max_dist) %>%
+  distinct() %>%
+  mutate(larvae_spacing = case_when(
+    min_dist > 192 ~ "not close enough",
+    min_dist <= 192 & min_dist > 32 ~ "close enough for high PLD",
+    min_dist <= 32 & min_dist > 8 ~ "close enough for high/medium PLD",
+    min_dist <= 8 ~ "close enough for all"
+  )) %>%
+  mutate(adult_spacing = case_when(
+    min_dist > 32 ~ "not close enough",
+    min_dist <= 32 & min_dist > 8 ~ "close enough for high movement",
+    min_dist <= 8 & min_dist > 0.5 ~ "close enough for high/medium movement",
+    min_dist <= 0.5 ~ "close enough for all"
+  )) %>%
+  mutate(larval_size = case_when(
+    size < 8 ~ "not large enough",
+    size >= 8 & size < 32 ~ "large enough for low PLD",
+    size >= 32 & size < 192 ~ "large enough for low/medium PLD",
+    size >= 192 ~ "large enough for all"
+  )) %>%
+  mutate(adult_size = case_when( # using 2 x home range rule
+    size < 0.5 * 2 ~ "not large enough",
+    size >= 0.5 * 2 & size < 8 * 2 ~ "large enough for low movement",
+    size >= 8 * 2 & size < 32 * 2 ~ "large enough for low/medium movement",
+    size >= 32 * 2 ~ "large enough for all"
+  ))
+
+sub_pisco <- list(sub_pisco, mpa_summary, wider_pisco) %>%
+  reduce(full_join)
+
+sub_pisco %>%
+  ungroup() %>%
+  select(sciname, movement) %>%
+  distinct() %>%
+  group_by(movement) %>%
+  summarize(n = n())
+
+
+# MPA Sup Fig -------------------------------------------------------------
+
+mpas <- full_pisco %>%
+  ungroup() %>%
+  select(affiliated_mpa, size, min_dist) %>%
+  distinct()
+
+ci <- st_read(here::here("data", "raw_data", "channel_islands.shp"))
+mpa1 <- st_read(here::here("data", "raw_data", "WDPA", "WDPA_shp_0", "WDPA_WDOECM_Nov2023_Public_1b091e2b51c258eb3b24328044a4a3d6979e00fa2029d7538aef2befd46da362_shp-polygons.shp")) %>%
+  st_transform(st_crs(ci))
+mpa2 <- st_read(here::here("data", "raw_data", "WDPA", "WDPA_shp_1", "WDPA_WDOECM_Nov2023_Public_1b091e2b51c258eb3b24328044a4a3d6979e00fa2029d7538aef2befd46da362_shp-polygons.shp")) %>%
+  st_transform(st_crs(ci))
+mpa3 <- st_read(here::here("data", "raw_data", "WDPA", "WDPA_shp_2", "WDPA_WDOECM_Nov2023_Public_1b091e2b51c258eb3b24328044a4a3d6979e00fa2029d7538aef2befd46da362_shp-polygons.shp")) %>%
+  st_transform(st_crs(ci))
+
+mpa_names1 <- as.data.frame(unique(mpa1$NAME))
+mpa_names2 <- as.data.frame(unique(mpa2$NAME))
+mpa_names3 <- as.data.frame(unique(mpa3$NAME))
+
+mpa1 <- mpa1 %>%
+  filter(NAME == "Anacapa Island State Marine Conservation Area" | grepl("Painted", NAME))
+mpa2 <- mpa2 %>%
+  filter(NAME == "Carrington Point (Santa Rosa Island) State Marine Reserve" |
+    NAME == "Gull Island (Santa Cruz Island) State Marine Reserve" |
+    NAME == "Harris Point (San Miguel Island) State Marine Reserve" |
+    NAME == "Anacapa Island State Marine Reserve" |
+    NAME == "Santa Barbara Island State Marine Reserve" |
+    NAME == "Scorpion (Santa Cruz Island) State Marine Reserve" |
+    NAME == "South Point (Santa Rosa Island) State Marine Reserve")
+mpa3 <- mpa3 %>%
+  filter(NAME == "Campus Point State Marine Conservation Area" |
+    NAME == "Naples State Marine Conservation Area")
+
+mpa_sf <- list(mpa1, mpa2, mpa3) %>%
+  reduce(rbind) %>%
+  janitor::clean_names() %>%
+  dplyr::select(name) %>%
+  mutate(affiliated_mpa = case_when(
+    name == "Anacapa Island State Marine Conservation Area" ~ "anacapa island smca",
+    name == "Painted Cave (Santa Cruz Island) State Marine Conservation Area" ~ "painted cave smca",
+    name == "Harris Point (San Miguel Island) State Marine Reserve" ~ "harris point smr",
+    name == "Carrington Point (Santa Rosa Island) State Marine Reserve" ~ "carrington point smr",
+    name == "South Point (Santa Rosa Island) State Marine Reserve" ~ "south point smr",
+    name == "Gull Island (Santa Cruz Island) State Marine Reserve" ~ "gull island smr",
+    name == "Scorpion (Santa Cruz Island) State Marine Reserve" ~ "scorpion smr",
+    name == "Santa Barbara Island State Marine Reserve" ~ "santa barbara island smr",
+    name == "Anacapa Island State Marine Reserve" ~ "anacapa island smr",
+    name == "Naples State Marine Conservation Area" ~ "naples smca",
+    name == "Campus Point State Marine Conservation Area" ~ "campus point smca"
+  )) %>%
+  full_join(mpas)
+
+rm(mpa1, mpa2, mpa3, mpa_names1, mpa_names2, mpa_names3)
+
+p1 <- ggplot() +
+  geom_sf(data = ci, fill = "transparent") +
+  geom_sf(data = mpa_sf, aes(fill = size)) +
+  theme_bw(base_size = 16) +
+  labs(fill = "MPA Size") +
+  scale_fill_viridis_c()
+
+p2 <- ggplot() +
+  geom_sf(data = ci, fill = "transparent") +
+  geom_sf(data = mpa_sf, aes(fill = min_dist)) +
+  theme_bw(base_size = 16) +
+  labs(fill = "MPA Spacing") +
+  scale_fill_viridis_c()
+
+p3 <- ggplot(mpas) +
+  geom_point(aes(size, min_dist)) +
+  theme_bw(base_size = 16) +
+  labs(x = "MPA Size", y = "MPA Spacing")
+
+plot = (p1 + p2) / p3 + plot_annotation(tag_levels = "A")
+
+ggsave(plot, filename = here::here("figs", "figS15.pdf"), height = 10, width = 12)
+
+# in/out figs --------------------------------------------------------------
+
+hline_df <- data.frame(hr_cat = c("low HR", "high HR"), val = c(0.5 * 2, 32 * 2)) %>%
+  mutate(hr_cat = as.factor(hr_cat)) %>%
+  mutate(hr_cat = fct_relevel(hr_cat, "low HR", "high HR"))
+
+col <- viridis::viridis(n = 9, end = 0.9)[c(1, 3, 9)]
+
+p2 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(size, in_out, color = movement)) +
+  geom_hline(aes(yintercept = 0), alpha = 0.5, linetype = "dashed") +
+  geom_vline(data = hline_df, aes(xintercept = val), color = "red", linetype = "dashed") +
+  # geom_vline(aes(xintercept = 8 * 2), color = "red", linetype = "dashed")+
+  # geom_vline(aes(xintercept = 32 * 2), color = "red", linetype = "dashed")+
+  # geom_line() +
+  geom_smooth(method = "lm", se = FALSE, linewidth = 2) +
+  theme_bw(base_size = 16) +
+  theme(strip.background = element_rect(fill = "transparent")) +
+  scale_color_manual(values = col) +
+  facet_wrap(~hr_cat, scales = "free_y") +
+  labs(y = "Log(In/Out)", x = "", color = "")
+p2 # make colors match colors from other fig
+
+ggsave(p2, filename = here::here("figs", "fig6.pdf"), height = 8, width = 10)
+
+p2 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(size, in_out, color = movement, group = sciname)) +
+  geom_hline(aes(yintercept = 0), alpha = 0.2, linetype = "dashed") +
+  geom_vline(aes(xintercept = 0.5 * 2), color = "red", linetype = "dashed") +
+  geom_vline(aes(xintercept = 8), color = "blue", linetype = "dashed") +
+  geom_vline(aes(xintercept = 32 * 2), color = "red", linetype = "dashed") +
+  # geom_line() +
+  geom_smooth(method = "lm", se = FALSE, linewidth = 2) +
+  theme_bw(base_size = 16) +
+  theme(strip.background = element_rect(fill = "transparent")) +
+  scale_color_manual(values = col) +
+  # facet_wrap(~movement, scales = "free_y") +
+  labs(y = "Log(In/Out)", x = "MPA Size", color = "")
+p2 # add icons
+
+ggsave(p2, filename = here::here("figs", "figS16.pdf"), height = 12, width = 12)
+
+p2 <- ggplot(sub_pisco %>% filter(site_status == "MPA"), aes(min_dist, in_out, color = movement, group = sciname)) +
+  geom_hline(aes(yintercept = 0), alpha = 0.2, linetype = "dashed") +
+  geom_vline(aes(xintercept = 0.5), color = "red", linetype = "dashed") +
+  geom_vline(aes(xintercept = 8), color = "blue", linetype = "dashed") +
+  geom_vline(aes(xintercept = 32), color = "red", linetype = "dashed") +
+  # geom_line() +
+  geom_smooth(method = "lm", se = FALSE, linewidth = 2) +
+  theme_bw(base_size = 16) +
+  theme(strip.background = element_rect(fill = "transparent")) +
+  scale_color_manual(values = col) +
+  # facet_wrap(~movement, scales = "free_y") +
+  labs(y = "Log(In/Out)", x = "MPA Spacing", color = "")
+p2 # add icons
+
+ggsave(p2, filename = here::here("figs", "figS17.pdf"), height = 12, width = 12)
